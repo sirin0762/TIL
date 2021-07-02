97091f2a875d4637a1f6ca240cc18be656772748
SSIN <60607880+sirin0762@users.noreply.github.com>
2021-07-02 21:36:25 +0900

    Update 7-8.java

--- algorithm/java/7-8.java
+++ algorithm/java/7-8.java
@@ -1,4 +1,5 @@
 // 떡 만들기
+// 내풀이
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -17,7 +18,7 @@ public class Main {
 				cut += arr[i] - mid;	
 			}
 		}
-		
+		// 이 방식 대로 할경우, 높이의 최대값이 나오지 않는다. 따라서 수정해야한다.
 		if(cut == target) {
 			return mid;
 		}
@@ -48,6 +49,56 @@ public class Main {
         }
         
         int result = binarySearch(arr, m, 0, end);
+        
+        System.out.println(result);
+        
+	}
+}
+
+// 답지
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	
+	public static int[] arr;
+	
+	public static void main(String[] args) {
+		// Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int start = 0;
+        int end = 0;
+        int result = 0;
+        
+        arr = new int[n];
+        
+        for(int i = 0; i < n; i++) {
+        	arr[i] = sc.nextInt();
+        	if(end < arr[i]) {
+        		end = arr[i];
+        	}
+        }
+        
+        while(start <= end) {
+        	int total = 0;
+        	int mid = (start + end) / 2;
+        	
+        	for(int i = 0; i < n; i++) {
+        		if(arr[i] > mid) total += arr[i] - mid;
+        	}
+        	
+        	if(total < m) {
+        		end = mid - 1;
+        	}
+        	else {
+        		result = mid;
+        		start = mid + 1;
+        	}
+        }
         
         System.out.println(result);
         
