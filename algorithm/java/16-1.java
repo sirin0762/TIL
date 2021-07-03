0092d732c1f870ed70ec8621fd770ada407e0a4b
SSIN <60607880+sirin0762@users.noreply.github.com>
2021-07-04 00:19:23 +0900

    Create 16-1.java

---
+++ algorithm/java/16-1.java
@@ -0,0 +1,41 @@
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	
+	public static int[][] arr;
+	
+	public static void main(String[] args) {
+		// Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		
+		arr = new int[n][m];
+		
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < m; j++) {
+				arr[i][j] = sc.nextInt();
+			}
+		}
+		
+		for(int i = 0; i < n; i++) {
+			for(int j = 1; j < m; j++) {
+				if(i == 0) {
+					arr[i][j] = arr[i][j] + Math.max(arr[i][j - 1], arr[i + 1][j - 1]);
+				}
+				else if(i == n - 1) {
+					arr[i][j] = arr[i][j] + Math.max(arr[i - 1][j - 1], arr[i][j - 1]);
+				}
+				else {
+					int temp = Math.max(arr[i - 1][j - 1], arr[i][j - 1]);
+					arr[i][j] = arr[i][j] + Math.max(temp, arr[i + 1][j - 1]);
+				}
+			}
+		}
+		
+		System.out.println(Arrays.deepToString(arr));
+	}
+}
